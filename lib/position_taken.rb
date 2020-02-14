814b62ad6c67c2535960039c043dd6abeae585d2
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-14 19:38:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board,input_of_index)
   if board == board
     return true
-    elsif Array[index_number] == NIL
+    elsif board == input_of_index
     return false
   end
 end
